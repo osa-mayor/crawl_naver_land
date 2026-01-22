edd94d1f2bfc5201a3bc842f90d5db87396086ec
Hongpyo Lee <oracle1127@gmail.com>
2026-01-22 21:05:03 +0900

    Fix: Update CSS selectors for Naver Land (Robust to hashed classes)

--- land_selectors.py
+++ land_selectors.py
@@ -1,15 +1,15 @@
 class NaverLandSelectors:
     # Region & Navigation
-    MORE_BUTTON = "button:has-text('더보기')"
+    MORE_BUTTON = "button[class*='button-more'], button:has-text('더보기')"
     
-    # Complex List Item
-    COMPLEX_ITEM = "li[class*='ComplexItem_article']"
+    # Complex List Item (CSS Module robustness)
+    COMPLEX_ITEM = "li[class*='ComplexItem'][class*='article']"
     
     # Within Complex Item
-    COMPLEX_LINK = "a[class*='ComplexItem_link']"
-    COMPLEX_NAME = "strong[class*='ComplexItem_name']"
-    COMPLEX_BADGE = "span[class*='TitleBadge_article']"
-    COMPLEX_INFO = "li[class*='ComplexItem_item-info']"
+    COMPLEX_LINK = "a[class*='ComplexItem'][class*='link'], a[class*='link']"
+    COMPLEX_NAME = "strong[class*='ComplexItem'][class*='name'], strong[class*='name']"
+    COMPLEX_BADGE = "span[class*='TitleBadge'][class*='article'], span[class*='badge']"
+    COMPLEX_INFO = "li[class*='ComplexItem'][class*='item-info'], li[class*='info']"
     
     # Detail Page (if used - currently mostly API intercept, but initial load check)
     # No specific detail page selectors used in crawler currently? 
